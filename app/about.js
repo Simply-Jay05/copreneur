6d54c7b8b0149eeb9997ecdb69b81619e333e394
Joseph Omale <mrjay0505@gmail.com>
2025-08-27 15:58:19 +0100

    added app description

--- app/about.js
+++ app/about.js
@@ -4,6 +4,7 @@ export default function About() {
     return (
         <View>
             <Text>About copreneur</Text>
+            <Text>AN app for developer to collaborate with enterpreneurs</Text>
         </View>
     );
 }
