209922ff4295f15d924265096e9459ed162b9c39
Joseph Omale <mrjay0505@gmail.com>
2025-08-30 21:19:24 +0100

    Signup screen design

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,25 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  const session = false;
+
   return (
     <Stack>
+      {session === true ?
+      <Stack.Screen 
+      name="(tabs)" 
+      options={{
+        title: "Home",
+        headerShown: false,
+      }}/>  
+      :
+      <Stack.Screen 
+      name="signup" 
+      options={{
+        title: "Create a new account",
+        headerShown: false,
+      }}/>
+    }
       <Stack.Screen 
       name="(tabs)" 
       options={{
@@ -14,13 +31,6 @@ export default function RootLayout() {
       name="index" 
       options={{
         title: "Welcome",
-        headerShown: false,
-      }}/>
-
-      <Stack.Screen 
-      name="signup" 
-      options={{
-        title: "Create a new account",
         headerShown: false,
       }}/>
 
--- app/signup.js
+++ app/signup.js
@@ -1,9 +1,56 @@
-import { Text, View } from "react-native";
+import { Image, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { colors } from "../theme/color";
 
 export default function Signup () {
     return(
-        <View>
-            <Text>Sign up Screen</Text>
+        <View style={styles.wrapper}>
+            {/* header group */}
+            <View style={styles.header}>
+                <Text style={styles.brandName}>copreneur</Text>
+                <Text style={styles.brandDesc}>Where entrepreneurs collaborate with developers</Text>
+            </View>
+
+            {/* {body group} */}
+            <View>
+                <Text>Create an account</Text>
+                <TouchableOpacity>
+                    <Image
+                    style={{
+                        width: 36,
+                        height: 36,
+                    }}
+                    source={require("../assets/images/google.png")}
+                    />
+                    <Text>Google</Text>
+                </TouchableOpacity>
+            </View>
+
+            {/* bottom group */}
+            <View></View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        backgroundColor: colors.brown200,
+        paddingTop: StatusBar.currentHeight,
+        paddingHorizontal: 16,
+    },
+    header: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 8,
+    },
+    brandName: {
+        fontSize: 46,
+        fontWeight: "bold",
+        colors: colors.brown400,
+    },
+    brandDesc: {
+        fontWeight: "semibold",
+        color: colors.brown400
+    }
+});
